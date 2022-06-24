3185b0ce19c9514d24dce8a3ca55388a76c03ba4
Jessy <c.j91350@gmail.com>
2022-06-24 16:49:19 +0200

    Remove mariadb folder

--- apps/user_service/db/ddl/init-data.sql
+++
@@ -1,2 +0,0 @@
-insert into user (userId, username, email)
-values (1, "jessy", "jessy@email.fr");
--- apps/user_service/db/ddl/init-table.sql
+++
@@ -1,8 +0,0 @@
-create table if not exists user (
-  userId int auto_increment not null,
-  firstname varchar(100) not null,
-  lastname varchar(100) not null,
-  email varchar(100) unique not null,
-  password varchar not null
-  primary key(userId)
-);
