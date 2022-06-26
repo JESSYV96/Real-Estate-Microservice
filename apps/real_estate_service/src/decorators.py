1ab3235dc4c77b9ec7d9a93658e7c647d2ab378a
Jessy <c.j91350@gmail.com>
2022-06-26 12:06:59 +0200

    feat: add token checking

--- apps/real_estate_service/src/decorators.py
+++ apps/real_estate_service/src/decorators.py
@@ -8,15 +8,24 @@ from src.helpers.jwt import get_payload_from_token
 def real_estate_owner(func):
     @wraps(func)
     def is_real_estate_owner(*args, **kwargs) -> bool:
-        current_owner = get_payload_from_token(
-            request.headers.get('Authorization'))
+        token = request.headers.get('Authorization')
+
+        if token is None:
+            raise Exception('Token not found')
+
+        current_owner = get_payload_from_token(token)
+
         owner_real_estate_id = RealEstate.query.get(
             kwargs['real_estate_id']).owner_id
+
         owner_id = current_owner['user_id']
+
         if owner_id is not owner_real_estate_id:
             raise PermissionError(
                 "You not allowed to edit this property, you are not the owner")
+
         result = func(*args, **kwargs)
+
         return result
 
     return is_real_estate_owner
